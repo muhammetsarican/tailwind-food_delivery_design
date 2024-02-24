7b88240feb7962d00db7a06951853b7c3d790264
muhammetsarican <muhammetsarican@gmail.com>
2024-02-25 00:49:13 +0300

    some fixes and login page created.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
   theme: {
     extend: {
+      container:{
+        center:true
+      },
       colors:{
         "primary":"#4E60FF",
         "secondary":"#EDEEF2",
