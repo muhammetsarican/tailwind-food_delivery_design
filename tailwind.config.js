bcab36a566adf01cae03feff06dce81fd0ec9c7d
muhammetsarican <muhammetsarican@gmail.com>
2024-02-24 15:49:14 +0300

    mobile button and menu added and some fixes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary":"#4E60FF",
+        "secondary":"#EDEEF2",
+        "tertiary":"#F8F9FF",
+        "quaternary":"#697BFF"
+      }
+    },
     fontFamily:{
       'nunito': 'Nunito, sans-serif'
     }
