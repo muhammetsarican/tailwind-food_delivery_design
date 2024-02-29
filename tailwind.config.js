b9e6cf957ae58eb0cc1c5728f745bb62a0c67485
muhammetsarican <muhammetsarican@gmail.com>
2024-02-29 23:57:19 +0300

    home page re-designed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
+  content: ["./index.html","./src/**/*.{html,js}", "./**/*.{html,js}"],
   theme: {
     extend: {
       container:{
